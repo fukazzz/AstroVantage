277a769e29bdaf5518949b2f6ff24923281f9edb
sidex15 <24408329+sidex15@users.noreply.github.com>
2025-12-14 10:14:04 +0800

    [BACKPORT] fs: implement susfs v2.1.0

--- fs/exec.c
+++ fs/exec.c
@@ -1725,7 +1725,7 @@ static int exec_binprm(struct linux_binprm *bprm)
 
 /*
  * sys_execve() executes a new program.
- */
+*/
 static int __do_execve_file(int fd, struct filename *filename,
 			    struct user_arg_ptr argv,
 			    struct user_arg_ptr envp,
--- kernel/reboot.c
+++ kernel/reboot.c
@@ -289,7 +289,7 @@ SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 	char buffer[256];
 	int ret = 0;
 
-#ifdef CONFIG_KSU 
+#ifdef CONFIG_KSU
 	ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
 #endif
 
